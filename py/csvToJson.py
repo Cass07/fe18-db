9fd49c4af3ccb07bf1d5b62fed1591077e8deedd
Cass07 <kjh95828@naver.com>
2026-09-14 21:07:13 +0900

    Initial commit

--- py/csvToJson.py
+++ py/csvToJson.py
@@ -2,20 +2,20 @@ import json
 import csv
 import os
 
-def csv_to_json(csv_file_path, json_file_path):
+def csv_to_json(_csv_file_path, _json_file_path):
     data = []
-    with open(csv_file_path, encoding='utf-8') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
+    with open(_csv_file_path, encoding='utf-8') as _csv_file:
+        csv_reader = csv.DictReader(_csv_file)
         for row in csv_reader:
             data.append(row)
 
-    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+    with open(_json_file_path, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
-    csv_folder = "../csv/"
-    json_folder = "../json/"
+    csv_folder = "csv/"
+    json_folder = "data/"
     # Convert all CSV files in the csv_folder to JSON files in the json_folder
     for csv_file in os.listdir(csv_folder):
         if csv_file.endswith(".csv"):
